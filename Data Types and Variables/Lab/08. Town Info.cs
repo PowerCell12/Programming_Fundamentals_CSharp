ab7a13b0a60c5a5660fd57b97a26c0ee1917da73
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-03-19 23:05:46 +0200

    Create 08. Town Info.cs

---
+++ Data Types and Variables/Lab/08. Town Info.cs
@@ -0,0 +1,5 @@
+string name = Console.ReadLine();
+    string population = Console.ReadLine();
+    string area = Console.ReadLine();
+
+    Console.WriteLine($"Town {name} has population of {population} and area {area} square km.");
