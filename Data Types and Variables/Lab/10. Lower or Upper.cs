8c84cafa2234e61110b8a14f05b842fcede2c7e3
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-03-19 23:06:20 +0200

    Create 10. Lower or Upper.cs

---
+++ Data Types and Variables/Lab/10. Lower or Upper.cs
@@ -0,0 +1,10 @@
+char character1 = char.Parse(Console.ReadLine());
+
+    if (Char.IsUpper(character1))
+    {
+      Console.WriteLine("upper-case");
+    }
+    else
+    {
+      Console.WriteLine("lower-case");
+    }
