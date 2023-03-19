f581ac8724594ce7ce9242bc93211c409ed0fcf0
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-03-19 23:05:16 +0200

    Create 06. Reversed Chars.cs

---
+++ Data Types and Variables/Lab/06. Reversed Chars.cs
@@ -0,0 +1,14 @@
+string line1 = Console.ReadLine();
+string line2 = Console.ReadLine();
+string line3 = Console.ReadLine();
+int counter = 0;
+
+while (counter != 1)
+{
+    Console.Write(line3);
+    Console.Write(" ");
+    Console.Write(line2);
+    Console.Write(" ");
+    Console.Write(line1);
+    counter += 1;
+}
