e8fb360116e4999ad2ac0ed98e2729a425f55218
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-03-19 23:06:01 +0200

    Create 09. Chars to String.cs

---
+++ Data Types and Variables/Lab/09. Chars to String.cs
@@ -0,0 +1,10 @@
+string character1 = Console.ReadLine();
+    string character2 = Console.ReadLine();
+    string character3 = Console.ReadLine();
+
+    for (int i = 0; i <= 0; i++)
+    {
+      Console.Write(character1);
+      Console.Write(character2);
+      Console.Write(character3);
+    }
