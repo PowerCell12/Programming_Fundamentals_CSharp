1465fb00eaf61b4fb0923f14dd46437da77930ba
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-06-29 11:17:50 +0300

    Create 07. NxN Matrix.cs

---
+++ Methods/Exercise/07. NxN Matrix.cs
@@ -0,0 +1,26 @@
+using System;
+
+partial class Program {  
+  public static void Main (string[] args) {    
+
+    int n   = int.Parse(Console.ReadLine());
+
+    Matrix(n);
+  }
+
+
+  static void Matrix(int n) { 
+
+    for (int i = 0; i < n; i++) {
+
+      for (int j = 0; j < n; j++) {
+
+        Console.Write($"{n} ");
+      }
+
+      Console.WriteLine();
+    }
+
+  }
+
+}
