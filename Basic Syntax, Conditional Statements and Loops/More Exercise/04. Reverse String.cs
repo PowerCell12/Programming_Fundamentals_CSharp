9f3d9fd90d562a389e0b04ea94cf90e6d44cc607
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-07-19 17:11:59 +0300

    Create 04. Reverse String.cs

---
+++ Basic Syntax, Conditional Statements and Loops/More Exercise/04. Reverse String.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string normal = Console.ReadLine();
+    string reversed = "";
+    
+    for (int i = normal.Length -1; i >= 0; i--)
+    {
+      reversed += normal[i];
+    }
+    Console.WriteLine(reversed);
+  }
+}
